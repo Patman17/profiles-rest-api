95efa7cbc49c154599e4c3f8a6141936e53c19c9
Patrick Ly <patrick.ly17@gmail.com>
2020-07-07 10:08:18 -0700

    PUT, PATCH and DELETE

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -2,4 +2,5 @@ from rest_framework import serializers
 
 class HelloSerialzier(serializers.Serializer):
     """ Serialzies a name field for testing our APIview"""
-    name = serializers.CharField()
+    name = serializers.CharField(max_length=10)
+    
